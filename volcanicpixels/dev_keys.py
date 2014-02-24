c889f17b054ceebbe74e6f4856125aaeb1421713
Daniel Chatfield <chatfielddaniel@gmail.com>
2014-02-24 17:16:44 +0000

    Remove sentry from dev version

--- volcanicpixels/dev_keys.py
+++ volcanicpixels/dev_keys.py
@@ -11,10 +11,6 @@ from flask.ext.volcano import is_dev_server
 if not is_dev_server():
     raise Exception("dev_keys.py cannot be used in production")
 
-
-SENTRY_DSN = ('appengine+http://49465b5619e34e1a9b72c9d59969ecd9'
-              '@app.getsentry.com/10299')
-
 SECRET_KEY = "NOTSOSECRETDEVKEY"
 
 CANONICAL_URL_ROOT = "https://www.volcanicpixels.com"
